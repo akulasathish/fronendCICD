58412d2553efc43bd180b1f569d46b44209166c0
akulasathish <71421128+akulasathish@users.noreply.github.com>
2024-10-25 16:45:24 +0530

    Update App.js1

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App!</h1>
+        <h1>Welcome to My first React App!</h1>
          <h1>created by sathish akula!</h1>
         <p>This React app is deployed using Jenkins and Docker.</p>
       </header>
