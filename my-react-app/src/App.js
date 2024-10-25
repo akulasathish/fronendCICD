20c56c01f291decf172d5e2760408571daa94af4
akulasathish <71421128+akulasathish@users.noreply.github.com>
2024-10-25 09:05:47 +0530

    Initial commit of React app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>This React app is deployed using Jenkins and Docker.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
